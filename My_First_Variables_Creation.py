449687cbfd225cc3657894a8f882028ac75c8a98
vijesh <konderu.vijesh@gmail.com>
2024-08-27 23:50:41 +0530

    Initial commit

---
+++ My_First_Variables_Creation.py
@@ -0,0 +1,34 @@
+#Adding Two Integers
+
+#Way 1
+a=20
+b=40
+print(a+b)
+
+#Way 2
+c=a+b
+print(c)
+
+#Way 3
+def func_1(a, b):
+    return a+b
+c= func_1()
+print(c)
+
+#Way 4
+print(func_1())
+
+#way 5
+def func_2(a, b):
+     c=a+b
+     print(c)
+
+#Way 6
+def add(a,b):
+    return func_2(a, b)
+
+# def add_two_numbers(a: int, b: int) -> Optional[int:
+#      print("sum of the two numbers or integers:", a+b)
+#      return None
+
+
